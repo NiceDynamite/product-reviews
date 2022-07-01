c174ac0900c477382868d4427564e9cf9e1de766
georgehniii <georgehniii@gmail.com>
2022-06-30 17:17:23 -0700

    Added express server with one get route for reviews

--- mini-mod-reviews/public/migration.sql
+++ mini-mod-reviews/public/migration.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS reviews;
-CREATE TABLE users (user_id SERIAL PRIMARY KEY, userName TEXT NOT NULL, pass TEXT NOT NULL, email Text);
-CREATE TABLE reviews (review_id SERIAL PRIMARY KEY, user_id INT REFERENCES users(user_id) NOT NULL, review_date TEXT NOT NULL, title TEXT NOT NULL, review TEXT NOT NULL, recommend BOOLEAN NOT NULL, rating INT NOT NULL, verified-purchaser BOOLEAN NOT NULL);
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (user_id INT PRIMARY KEY, userName TEXT NOT NULL, pass TEXT NOT NULL, email Text);
+CREATE TABLE reviews (review_id SERIAL PRIMARY KEY, user_id INT REFERENCES users(user_id) NOT NULL, review_date TEXT NOT NULL, title TEXT NOT NULL, review TEXT NOT NULL, recommend BOOLEAN, rating INT NOT NULL, verified_purchaser BOOLEAN NOT NULL);
